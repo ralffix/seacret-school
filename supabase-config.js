e61d841d6d3afacd3bdc62ba8d30b88e485b6629
ralffix <ralfs.f@outlook.com>
2025-09-11 14:40:18 +0300

    a

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,3 @@
 // DON'T commit this file to GitHub!
-window.SUPABASE_URL = 'https://ojodoocwsgbprgxndtmp.supabase.co';
+window.SUPABASE_URL = 'https://cvjaqbghngnuhznxqkgn.supabase.co';
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2amFxYmdobmdudWh6bnhxa2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MzM5NjYsImV4cCI6MjA3MzAwOTk2Nn0.24Gx5caICw8WB_E8_qrZDzEleTwIKPOFuZKhO8pREL8';
-    
